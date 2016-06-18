62d97795e32272290436c42f7e59e80c1848e174
George G <ggogarten@gmail.com>
2016-06-18 08:49:25 -0400

    updating GPS data

--- Where Am I/ViewController.swift
+++ Where Am I/ViewController.swift
@@ -7,12 +7,70 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var manager: CLLocationManager!
+    
+    
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        
+        manager.delegate = self
+        
+        manager.desiredAccuracy - kCLLocationAccuracyBest
+        
+        manager.requestWhenInUseAuthorization()
+        
+        manager.startUpdatingLocation()
+        
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        print(locations)
+        
+        var userLocation:CLLocation = locations[0]
+        
+            self.latitudeLabel.text = "\(userLocation.coordinate.latitude)"
+        
+            self.longitudeLabel.text = "\(userLocation.coordinate.longitude)"
+        
+            self.courseLabel.text = "\(userLocation.course)"
+        
+            self.speedLabel.text = "\(userLocation.speed)"
+        
+            self.altitudeLabel.text = "\(userLocation.altitude)"
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
